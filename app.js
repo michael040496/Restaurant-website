15b1a2bcedb595784fe3d34fad9d7388f429fda1
Philippe Rasay <guerrerophilippe@gmail.com>
2019-03-20 14:08:20 +0100

    no message

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 var path = require('path');
-//const db = require('./models/db.js/')
 const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
