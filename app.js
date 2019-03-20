25dbc2fb5d06684a1e2816ba0176700b38b6eb0a
Philippe Rasay <guerrerophilippe@gmail.com>
2019-03-20 13:24:44 +0100

    added index.html

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ import router from './routes/index.js';
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+var path = require('path');
 //const db = require('./models/db.js/')
 const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+ });
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
